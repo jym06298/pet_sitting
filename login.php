97c0eea52b534c40bdf15942079786b454d5e5ba
jym06298 <42586055+jym06298@users.noreply.github.com>
2022-04-27 12:38:39 -0400

    stupid bug syntax error

--- login.php
+++ login.php
@@ -1,14 +1,32 @@
-<html>
-<head></head>
+<?php
+    session_start();
+    if ( isset($_POST['submit']) ){
+        $login_query = "SELECT * FROM customers, employees WHERE ";
+    } else {
+        echo "Please log-in";
+    }
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+
+
+
 <body>
-
-<h2>Login Form</h2>
-<form action="/action_page.php"><label for="username">Username:</label><br /><input 
-id="username" name="username" type="text" /><br /><br /><label 
+    <h2>Login Form</h2>
+<form method = "post" action="#">
+    <label for="email">Email:</label> <br /><input 
+id="email" name="email" type="text" /><br /><br /><label 
 for="password">Password:</label><br /><input id="password" name="password" 
-type="text" /><br /><br /><input type="submit" value="Submit" /></form> 
-
-
+type="text" /><br /><br /><input type="submit" value="Submit" />
+</form> 
 
 </body>
 </html>
---
+++ login_1.php
@@ -0,0 +1,21 @@
+<?php
+    require('database.php');
+    session_start();
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="stylesheet" href="pet_sitting.css">
+    <title>Animal Profile Form</title>
+</head>
+<body>
+ <div>hello</div>
+
+
+</body>
+</html>
