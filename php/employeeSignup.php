d1293b71794f738486557378618ac3af6a3e7da3
jym06298 <42586055+jym06298@users.noreply.github.com>
2022-05-01 23:51:17 -0400

    put updated topnav into all files

--- php/employeeSignup.php
+++ php/employeeSignup.php
@@ -101,13 +101,25 @@
         <h1>DBMS Petsitting Co.</h1>
     </div>
 	<div class="topnav">
-		<ul>
-			<li><a href="homepage.php">Home</a></li>
-			<li><a class="active" href="employeeSignup.php">Employee Sign-Up</a></li>
-			<li><a href="customerSignUp.php">Customer Sign-Up</a></li>
-			<li><a href="login.php">Login</a></li>
-			<li><a href="logout.php">Logout</a></li>
-		</ul>
+    <ul>
+		<li><a href="homepage.php">Home</a></li>
+    <!--SHOULD ADD LOGIC WHERE IT WILL GO TO EMPLOYEE PROFILE OR CUSTOMER PROFILE -->
+    <li><a href= <?php
+                if ($_SESSION['isEmployee']) {
+                    echo "employeeProfile.php";
+                } else {
+                    echo "customerProfile.php";
+                } //if else
+      ?> >Profile</a> </li>
+		<li><a href="employeeSignup.php">Employee Sign-Up</a></li>
+		<li><a href="customerSignUp.php">Customer Sign-Up</a></li>
+        <li><a href="animalSignup.php">Create Pet Account</a></li>
+        <li><a href="createPosts.php">Create Post</a></li>
+        <li><a href="posts.php">Posts</a></li>
+		<li><a href="login.php">Login</a> </li>
+		<li><a href="logout.php">Logout</a></li>
+    </ul>
+	</div><br>
 	</div>
     <form action="#" method = "post">
         <label for="fname">First name:</label>
