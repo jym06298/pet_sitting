54eb44362a3957a111bb76c6feffe393fda492a5
jym06298 <42586055+jym06298@users.noreply.github.com>
2022-05-10 12:08:37 -0400

    Update triggers.sql
    added trigger

--- sql/triggers.sql
+++ sql/triggers.sql
@@ -1,4 +1,6 @@
 DROP TRIGGER IF EXISTS update_cost;
+DROP TRIGGER IF EXISTS update_orders_employee_delete;
+
 DELIMITER //
 
 CREATE TRIGGER update_cost
@@ -9,4 +11,10 @@ BEGIN
     SET NEW.cost = get_cost(NEW.begin_time, NEW.end_time, NEW.employeeID);
 END //
 
+CREATE TRIGGER update_orders_employee_delete
+AFTER DELETE
+ON employee FOR EACH ROW
+BEGIN
+    UPDATE orders SET employeeID = NULL, cost = NULL WHERE employeeID = OLD.employeeID;
+END //
 DELIMITER ;
